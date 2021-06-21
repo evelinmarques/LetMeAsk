0bbdb7f13fd6071082ddd90f4c0214142e26ec72
evelinmarques <contatoevelinmarques@hotmail.com>
2021-06-21 12:22:32 -0300

    fix(src): fix images

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,20 @@
-// import illustrationImg from '../assets/img/illustration.svg';
-// import logoImg from '../assets/img/logo.svg';
-// import googleIconImg from '../assets/img/google-icon.svg';
+import illustrationImg from '../assets/img/illustration.svg';
+import logoImg from '../assets/img/logo.svg';
+import googleIconImg from '../assets/img/google-icon.svg';
 
 export function Home() {
   return (
     <div>
       <aside>
-        {/* <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" /> */}
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
         <strong>Crie salas de Q&amp;A ao vivo</strong>
         <p>Tire as dúvidas da sua audiência em tempo-real</p>
       </aside>
       <main>
         <div>
-          {/* <img src={logoImg} alt="Letmeask" /> */}
+          <img src={logoImg} alt="Letmeask" />
           <button>
-            {/* <img src={googleIconImg} alt="Logo do Google" /> */}
+            <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
           <div>ou entre em uma sala</div>
